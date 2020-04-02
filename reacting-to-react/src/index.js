b4811ad0d6cc86a21c7372abccd5b9d72099d06a
Paula Suarez <paula.suarez@freeman.com>
2020-04-02 18:58:16 -0500

    Adding updtes to repo.

---
+++ reacting-to-react/src/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App name="Lover"/>, document.getElementById('root'));
+
+ const Name = (props) => {
+    return (
+          'Text!'
+     );
+};
+export default Name;
